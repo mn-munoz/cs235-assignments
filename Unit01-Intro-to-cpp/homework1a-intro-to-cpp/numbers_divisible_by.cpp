a3319bc72e8fadbcd3a5a54bd9f6eb48ece975eb
Merleth <merlethmunozra@gmail.com>
2024-01-16 11:40:32 -0700

    starting homework 1

--- Unit01-Intro-to-cpp/homework1a-intro-to-cpp/numbers_divisible_by.cpp
+++ Unit01-Intro-to-cpp/homework1a-intro-to-cpp/numbers_divisible_by.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <string>
 
+using std::cout, std::endl;
+
 int main(int argc, char const* argv[]) {
     // Write your code here
+    int start = atoi(argv[0]);
+    int end = atoi(argv[1]);
+    int divisor = atoi(argv[2]);
+
+    cout << "hello" << endl;
+
 
     return 0;
 }
