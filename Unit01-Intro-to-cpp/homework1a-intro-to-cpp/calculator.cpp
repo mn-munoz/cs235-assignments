b4fe36842e20eb5c8da8279bd9f0b9aa26e2a0ff
Merleth <merlethmunozra@gmail.com>
2024-01-17 13:04:21 -0700

    added to makefile, complete  calculator.cpp

--- Unit01-Intro-to-cpp/homework1a-intro-to-cpp/calculator.cpp
+++ Unit01-Intro-to-cpp/homework1a-intro-to-cpp/calculator.cpp
@@ -9,8 +9,43 @@ std::string input(std::string prompt) {
     return response;
 }
 
+using std::cout, std::endl;
+
 int main(int argc, char const* argv[]) {
     // Write your code here
+
+
+    while (true) {
+        std::string operation = input("operation: ");
+
+        if (!operation.length()) {
+            break;
+        }
+        else if (operation != "add" && operation != "subtract" && operation != "multiply" && operation != "divide" && operation != "mod") {
+            cout << operation << " isn't a valid operation" << endl;
+            continue;
+        }
+        
+        int lOperand = stoi(input("left operand: "));
+        int rOperand = stoi(input("right operand: "));
+
+        if (operation == "add") {
+            cout << lOperand + rOperand << endl;
+        }
+        else if (operation == "subtract") {
+            cout << lOperand - rOperand << endl;
+        }
+        else if (operation == "multiply") {
+            cout << lOperand * rOperand << endl;
+        }
+        else if (operation == "divide") {
+            cout << lOperand / rOperand << endl;
+        }
+        else if (operation == "mod") {
+            cout << lOperand % rOperand << endl;
+        }
+    }
+    
 
     return 0;
 }
