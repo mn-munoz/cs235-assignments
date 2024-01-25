b99c22dc2c07f7b329109891df2c1fc63f01a3de
Merleth <merlethmunozra@gmail.com>
2024-01-25 14:09:29 -0700

    complete reformat.cpp

--- Unit01-Intro-to-cpp/homework1b-file-io/reformat.cpp
+++ Unit01-Intro-to-cpp/homework1b-file-io/reformat.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+
+using std::cerr, std::cout, std::endl;
+using std::ifstream, std::ofstream;
+using std::string;
 
 int main(int argc, char const* argv[]) {
-    // Write your code here
+    ifstream inFile;
+    ofstream outFile;
+    string inputName = argv[1];
+    string outName = argv[2];
+    inFile.open(inputName);
+    outFile.open(outName);
 
+    if (!inFile.is_open()) {
+        cerr << "File given could not open" << endl;
+        exit(1);
+    }
+
+    while(!inFile.eof()) {
+        string name;
+        string lastName; 
+        int points;
+        double factor;
+        inFile >> name >> lastName >> points >> factor; 
+
+        if (name.empty() && lastName.empty()) {
+            break;
+        }
+
+        outFile << lastName << ", " << name << ": " << points * factor << endl;
+    }
+
+    outFile.close();
+    inFile.close();
     return 0;
 }
--- Unit02-STL-data-structures/lecture2a-vector-set/user_registration.cpp
+++ Unit02-STL-data-structures/lecture2a-vector-set/user_registration.cpp
@@ -1,1 +1,22 @@
-// :)
+#include <set>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    set<string> IDs;
+    cout << "Enter an ID: ";
+    string id;
+    
+    while (getline(cin, id)) {
+        if (id.empty()) { break; }
+        if (IDs.find(id) == IDs.end()) {
+            IDs.insert(id);
+            // More to do
+        } else {
+            cout << "ID already in use" << endl;
+        }
+    }
+    return 0;
+}
