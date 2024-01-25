4eb073cc7ed43b73ddbfcc9106386561cc8283ff
Merleth <merlethmunozra@gmail.com>
2024-01-25 10:23:12 -0700

    completed grocery.cpp

--- Unit02-STL-data-structures/lecture2a-vector-set/grocery.cpp
+++ Unit02-STL-data-structures/lecture2a-vector-set/grocery.cpp
@@ -1,1 +1,30 @@
-// :)
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+    // Get the list
+    vector<string> groceries;
+    cout << "Enter item: ";
+    string item;
+    while(getline(cin, item)) {
+        if (item == "") {
+            cout << "Ending the list" << endl;
+            break;
+        }
+        groceries.push_back(item);
+        cout << "Enter item: ";
+    }
+    // print the list
+    cout << "GROCERY LIST:" << endl;
+
+    for (auto item: groceries) {
+        cout << item << endl;
+    }
+
+    // for (size_t i = 0; i < groceries.size(); i++) {
+    //     cout << groceries.at(i) << endl;
+    // }
+    return 0;
+}
