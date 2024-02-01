00901a5d6df9857faf1ee9c75e9029e3e3dbba51
Merleth <merlethmunozra@gmail.com>
2024-02-01 09:47:41 -0700

    Added Vector.h

---
+++ Unit02-STL-data-structures/lecture2c-queue-stack-pqueue/Vector.h
@@ -0,0 +1,26 @@
+#pragma once
+
+template<class T>
+class Stack {
+    private:
+        vector<T> m_data;
+    public:
+        size_t size() {
+            return m_data.size();
+        }
+
+        bool empty() {
+            return m_data.empty();
+        }
+
+        void push(const t& item) {
+            m_data.push_back(item);
+        }
+
+        void pop() {
+            m_data.pop_back();
+        }
+        T& top() {
+            return m_data.at(m_data.size()-1);
+        }
+};
