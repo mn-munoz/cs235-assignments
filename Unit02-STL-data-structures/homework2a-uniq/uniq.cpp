b2099320b951193fe276cc8f7be6d3912785ca19
Merleth <merlethmunozra@gmail.com>
2024-02-27 10:00:21 -0700

    finish implementing VectorSet.h

--- Unit02-STL-data-structures/homework2a-uniq/uniq.cpp
+++ Unit02-STL-data-structures/homework2a-uniq/uniq.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <set>
 
-int main(int argc, char const* argv[]) {
-    if (/* condition */)
-    {
-        /* code */
+using std::string, std::cerr, std::cout, std::endl, std::cin;
+using std::ifstream, std::istream;
+using std::set;
+
+void readInput(set<string>& data, istream& ins = cin) {
+    string line;
+    while(getline(ins,line)) {
+        if (!(data.find(line) != data.end())) {
+            cout << line << endl;
+            data.insert(line);
+        }
     }
-    
+}
 
+ 
+int main(int argc, char const* argv[]) {
+    set<string> data;
+    if (argc == 2) {
+        string fileName = argv[1];
+        ifstream file;
+        file.open(fileName);
+
+        if (!file.is_open()) {
+            cerr << "File could not be open" << endl;
+            exit(2);
+        }
+
+        readInput(data, file);
+        file.close();
+    }
+    else {
+        readInput(data);
+    }
     return 0;
 }
